437bace9fd28af7505a6e3e51426debd734871de
Yana Sirkich <yanasplanet@gmail.com>
2024-10-13 13:46:02 +0300

    small typo fix

--- srcs/fractal.c
+++ srcs/fractal.c
@@ -6,7 +6,7 @@
 /*   By: ysirkich <ysirkich@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 10:01:53 by ysirkich          #+#    #+#             */
-/*   Updated: 2024/10/13 13:37:26 by ysirkich         ###   ########.fr       */
+/*   Updated: 2024/10/13 13:45:46 by ysirkich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ t_fractol	fractal_init(char *name)
 	fractal = malloc(sizeof(t_fractol));
 	if (!fractal)
 		error("Error. Memory allocation failed.", NULL);
-	if (ft_strcmp(name, "Mandelbrot") == 0)
+	if (ft_strcmp(name, "Mandelbrot") == 0 || ft_strcmp(name, "mandelbrot") == 0)
 		mandelbrot_init(fractal);
-	if (ft_strcmp(name, "Julia") == 0)
+	if (ft_strcmp(name, "Julia") == 0 || ft_strcmp(name, "julia") == 0)
 		julia_init(fractal);
 	else
 	{
-		ft_putendl_fd("Error. Wrong input. Usage: ./fractol [julia] or [mandelbrot]", 1);
+		ft_putendl_fd("Error. Wrong input. Usage: ./fractol [Julia] or [Mandelbrot]", 1);
 		return (NULL);
 	}
 	return (0);
