7cac18d29e9db74f82b495f30d6d1e57a536e6b7
Яна <yanasplanet@gmail.com>
2024-10-23 11:58:46 +0300

    mandelbrot function somewhat done

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: ysirkich <ysirkich@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 10:00:12 by ysirkich          #+#    #+#             */
-/*   Updated: 2024/10/22 12:12:32 by ysirkich         ###   ########.fr       */
+/*   Updated: 2024/10/23 11:58:02 by ysirkich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,15 @@
 
 # define WIDTH 1280
 # define HEIGHT 720
+
+#define BLACK 0x000000
+#define WHITE 0xFFFFFF
+#define RED   0xFF0000
+#define GREEN 0x00FF00
+#define BLUE  0x0000FF
+#define YELLOW 0xFFFF00
+#define CYAN   0x00FFFF
+#define MAGENTA 0xFF00FF
 
 typedef struct s_fractol
 {
--- srcs/fractal.c
+++ srcs/fractal.c
@@ -6,7 +6,7 @@
 /*   By: ysirkich <ysirkich@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 10:01:53 by ysirkich          #+#    #+#             */
-/*   Updated: 2024/10/22 12:28:32 by ysirkich         ###   ########.fr       */
+/*   Updated: 2024/10/23 11:57:43 by ysirkich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ t_fractol	fractal_init(char *name)
 	t_fractol	*fractal;
 
 	fractal = malloc(sizeof(t_fractol));
+	if (!fractal)
+		error("Error. Memory allocation failed.", NULL);
 	fractal->zoom = 1.0;
 	fractal->offset_x = 0;
 	fractal->offset_y = 0;
 	fractal->max_iterations = 50;
-	if (!fractal)
-		error("Error. Memory allocation failed.", NULL);
 	if (ft_strcmp(name, "Mandelbrot") == 0 || ft_strcmp(name, "mandelbrot") == 0)
 		fractal->type = MANDELBROT;
 	if (ft_strcmp(name, "Julia") == 0 || ft_strcmp(name, "julia") == 0)
@@ -37,17 +37,45 @@ t_fractol	fractal_init(char *name)
 }
 
 int	*mandelbrot_pixel(t_fractol *fractal, int x, int y)// iterate over the mandelbrot equation and determine how quickly the point escapes
-{
+{ //z = z^2 + c z 
 	double	real;
 	double	imaginary;
+
 	double	z_real;
 	double	z_imaginary;
 
-	real = (x - 400) / (200.0 * fractal->zoom) + fractal->offset_x;
-	imaginary = (y - 300) / (200.0 * fractal->zoom) + fractal->offset_y;
-	//z to 0 + 0i
+	double	z_sq_real;
+	double	z_sq_imag;
+
+	double	c_real;
+	double	c_imaginary;
+	
+	int		iterations;
+
+	real = (x - (WIDTH / 2)) / ((WIDTH / 4) * fractal->zoom) + fractal->offset_x;
+	imaginary = (y - (HEIGHT / 2)) / ((HEIGHT / 4) * fractal->zoom) + fractal->offset_y;
+	//initialize z to 0 + 0i
 	z_real = 0.0;
 	z_imaginary = 0.0;
+	z_sq_real - 0.0;
+	z_sq_imag = 0.0;
+	iterations = 0;
+	c_real = real;
+	c_imaginary = imaginary;
+	//z = z^2 + c iteration
+	while (z_sq_real + z_sq_imag < 4 && fractal->max_iterations)
+	{
+		z_imaginary = 2 * z_real * z_imaginary + c_imaginary;
+		z_real = z_sq_real - z_sq_imag + c_real;
+		z_sq_real = z_real * z_real;
+		z_sq_imag = z_imaginary * z_imaginary;
+		iterations++;
+	}
+	if (iterations == fractal->max_iterations)
+		return(BLACK);
+	else
+		return (GREEN * iterations / fractal->max_iterations);
+
 }
 
 //int	*julia_pixel(t_fractol *fractal, int x, int y)
